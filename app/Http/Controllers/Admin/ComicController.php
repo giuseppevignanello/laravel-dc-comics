4d0b238a471def971932a56f24976a6f05478420
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-29 15:12:25 +0200

    add index, create and show methods

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -2,8 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
+
+    public function index()
+    {
+        return view('admin.comics.index');
+    }
+
+    public function create()
+    {
+        return view('admin.comics.create');
+    }
+    public function show(Comic $comic)
+    {
+        return view('admin.comics.show', compact('comic'));
+    }
 }
