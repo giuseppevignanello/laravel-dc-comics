a575f42a67613a28cf42c663649a4bb227904077
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-31 17:38:02 +0200

    add about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container text-white">
+        <h2 class="mt-3"> Hi </h2>
+        <h3> My name is Giuseppe Vignanello </h3>
+        <p>
+            I'm a Junior Full Stack Web Developer, born in Sicily and resident in
+            Barcelona. <br>
+            This website is made with Laravel 9.2
+        </p>
+    </div>
 @endsection
