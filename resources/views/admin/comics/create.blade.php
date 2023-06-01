3dca630a88547d29302dd4971efd62f101f06532
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-01 12:04:56 +0200

    show old value in case of error

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -18,7 +18,7 @@
                 <input type="text" class="form-control @error('title')
                 is-invalid
             @enderror"
-                    id="title" name="title">
+                    id="title" name="title" value="{{ old('title') }}">
                 @error('title')
                     <div class="alert alert-danger" role="alert">
                         <h4 class="alert-heading">Title, Error: {{ $message }}</h4>
@@ -27,18 +27,19 @@
             </div>
             <div class="col-md-4">
                 <label for="description" class="form-label text-white">Description</label>
-                <input type="text" class="form-control" id="description" name="description">
+                <input type="text" class="form-control" id="description" name="description"
+                    value="{{ old('description') }}">
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label text-white">Image</label>
-                <input class="form-control" type="text" id="thumb" name="thumb">
+                <input class="form-control" type="text" id="thumb" name="thumb" value="{{ old('thumb') }}">
             </div>
             <div class="col-md-4">
                 <label for="price" class="form-label text-white">Price</label>
                 <input type="text" class="form-control @error('price')
                 is-invalid
             @enderror"
-                    id="price" name="price">
+                    id="price" name="price" value="{{ old('price') }}">
             </div>
             @error('price')
                 <div class="alert alert-danger" role="alert">
@@ -50,7 +51,7 @@
                 <input type="text" class="form-control @error('series')
                 is-invalid
             @enderror"
-                    id="series" name="series">
+                    id="series" name="series" value="{{ old('series') }}">
             </div>
             @error('series')
                 <div class="alert alert-danger" role="alert">
@@ -59,14 +60,14 @@
             @enderror
             <div class="col-md-4">
                 <label for="sale_date" class="form-label text-white">Sale Date</label>
-                <input type="date" class="form-control" id="sale_date" name="sale_date">
+                <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date') }}">
             </div>
             <div class="col-md-4">
                 <label for="type" class="form-label text-white">Type</label>
                 <input type="text" class="form-control @error('type')
                 is-invalid
             @enderror"
-                    id="type" name="type">
+                    id="type" name="type" value="{{ old('type') }}">
             </div>
             @error('series')
                 <div class="alert alert-danger" role="alert">
